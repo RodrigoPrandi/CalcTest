6224a057135218ff8f1f5428923200a4ca1709f8
Rodrigo Prandi <rodrigo.prandi@benner.com.br>
2018-11-13 21:47:40 -0200

    Controller ShowMeTheCode inicial

---
+++ CalcTest.WebApi/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalcTest.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowMeTheCodeController : ControllerBase
+    {
+        // GET: api/ShowMeTheCode
+        [HttpGet]
+        public string Get()
+        {
+            return "https://github.com/RodrigoPrandi/CalcTest";
+        }
+    }
+}
