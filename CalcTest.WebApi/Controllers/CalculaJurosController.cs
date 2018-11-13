7422c709f3794d446c99cf87cb5b131509fdff1f
Rodrigo Prandi <rodrigo.prandi@benner.com.br>
2018-11-13 21:52:10 -0200

    Controller inicial Calcula Juros

---
+++ CalcTest.WebApi/Controllers/CalculaJurosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalcTest.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculaJurosController : ControllerBase
+    {
+        // GET: api/ShowMeTheCode
+        [HttpGet]
+        public decimal Get(decimal valorInicial, int meses)
+        {
+            return 0;
+        }
+    }
+}
