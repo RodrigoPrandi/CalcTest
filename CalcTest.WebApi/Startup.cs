c7450e457538cc5e901d808af271ca15eca76f04
Rodrigo Prandi <rodrigo.prandi@benner.com.br>
2018-11-13 21:12:53 -0200

    Configurando swagger

--- CalcTest.WebApi/Startup.cs
+++ CalcTest.WebApi/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CalcTest.WebApi
 {
@@ -26,6 +20,18 @@ namespace CalcTest.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "CalcTest WebApi",
+                    Description = "WebApi contendo endpoints para avaliação do novo SoftPlayer!",
+                    Contact = new Contact { Name = "Rodrigo Prandi Araújo", Email = "rodrigo.prandi@outlook.com" }
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +48,13 @@ namespace CalcTest.WebApi
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "CalcTest WebApi");
+            });
         }
     }
 }
