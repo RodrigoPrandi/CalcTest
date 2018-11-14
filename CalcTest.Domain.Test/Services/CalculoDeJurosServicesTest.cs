ed2d7b7dffb8e7e2cd524a08a967f500939a90ae
Rodrigo Prandi <rodrigo.prandi@benner.com.br>
2018-11-14 21:28:00 -0200

    Testes Classe CalculoDeJurosServices

---
+++ CalcTest.Domain.Test/Services/CalculoDeJurosServicesTest.cs
@@ -0,0 +1,43 @@
+﻿using CalcTest.Domain.Business.Factory.Interfaces;
+using CalcTest.Domain.Business.Interfaces;
+using CalcTest.Domain.Services;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcTest.Domain.Test.Services
+{
+    [TestFixture]
+    class CalculoDeJurosServicesTest
+    {
+        private const decimal VALORINICIAL = 105;
+        private const int QUANTIDADEMESES = 5;
+        private const decimal RESULTADO = 106;
+
+        [Test]
+        public void DeveConstruirRegraDeNegocioPeloCalculoDeJurosFactory()
+        {
+            var calculoDeJurosFactoryMock = Substitute.For<ICalculoDeJurosFactory>();
+
+            var calculoDeJurosService = new CalculoDeJurosServices(calculoDeJurosFactoryMock);
+
+            calculoDeJurosFactoryMock.Received(1).Build();
+        }
+
+        [Test]
+        public void MetodoCalcularDoServicoDeverRetornarOResultadoDoMetodoCalcularDaRegraDeNegocioCriadaPeloFactory()
+        {
+            var calculoDeJurosFactoryMock = Substitute.For<ICalculoDeJurosFactory>();
+            var calculoDeJurosBusinessMock = Substitute.For<ICalculoDeJuros>();
+            calculoDeJurosBusinessMock.Calcular(VALORINICIAL, QUANTIDADEMESES).Returns(RESULTADO);
+            calculoDeJurosFactoryMock.Build().Returns(calculoDeJurosBusinessMock);
+
+            var calculoDeJurosService = new CalculoDeJurosServices(calculoDeJurosFactoryMock);
+            var resultadoServico = calculoDeJurosService.Calcular(VALORINICIAL, QUANTIDADEMESES);
+
+            Assert.AreEqual(RESULTADO, resultadoServico);
+        }
+    }
+}
--- CalcTest.Domain/Services/CalculoDeJurosServices.cs
+++ CalcTest.Domain/Services/CalculoDeJurosServices.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+using CalcTest.Domain.Business.Factory.Interfaces;
 using CalcTest.Domain.Services.Interfaces;
 
 namespace CalcTest.Domain.Services
 {
+    /// <summary>
+    /// Serviço de cálculo de juros compostos
+    /// </summary>
     public class CalculoDeJurosServices : ICalculoDeJurosServices
     {
-        public CalculoDeJurosServices()
+        private readonly ICalculoDeJurosFactory _calculoDeJurosFactory;
+
+        /// <summary>
+        /// Construtor do serviço cálculo de juros
+        /// </summary>
+        /// <param name="calculoDeJurosFactory">Factory responsável por criar a regra de cálculo de juros</param>
+        public CalculoDeJurosServices(ICalculoDeJurosFactory calculoDeJurosFactory)
         {
-            
+            _calculoDeJurosFactory = calculoDeJurosFactory;
         }
+
+        /// <summary>
+        /// Realiza o cálculo do valor todal de juros a partir de um valor inicial
+        /// e uma quantidade de meses especificada
+        /// </summary>
+        /// <param name="valorInicial">Valor inial a ser aplicado o juros </param>
+        /// <param name="quantidadeMeses">Quantidade de meses </param>
+        /// <returns></returns>
         public decimal Calcular(decimal valorInicial, int quantidadeMeses)
         {
             throw new NotImplementedException();
