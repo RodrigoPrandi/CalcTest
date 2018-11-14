5a838e9e1a8c8f89d2f8b70e1c2bfed0f90e76c9
Rodrigo Prandi <rodrigo.prandi@benner.com.br>
2018-11-14 21:44:11 -0200

    Implementando CalculoDeJurosServices para passar nos teste

--- CalcTest.Domain/Services/CalculoDeJurosServices.cs
+++ CalcTest.Domain/Services/CalculoDeJurosServices.cs
@@ -21,15 +21,16 @@ namespace CalcTest.Domain.Services
         }
 
         /// <summary>
-        /// Realiza o cálculo do valor todal de juros a partir de um valor inicial
+        /// Realiza o cálculo do valor total de juros a partir de um valor inicial
         /// e uma quantidade de meses especificada
         /// </summary>
         /// <param name="valorInicial">Valor inial a ser aplicado o juros </param>
-        /// <param name="quantidadeMeses">Quantidade de meses </param>
-        /// <returns></returns>
+        /// <param name="quantidadeMeses">Quantidade de meses a ser aplicado o juros</param>
+        /// <returns>Valor final do calculo de juros pelos meses informado</returns>
         public decimal Calcular(decimal valorInicial, int quantidadeMeses)
         {
-            throw new NotImplementedException();
+            var calculoDeJurosBusiness = _calculoDeJurosFactory.Build();
+            return calculoDeJurosBusiness.Calcular(valorInicial, quantidadeMeses);
         }
     }
 }
