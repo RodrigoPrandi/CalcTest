366ead1c926efa96a9e39492f293ac86f0583306
Rodrigo Prandi <rodrigo.prandi@benner.com.br>
2018-11-15 10:13:15 -0200

    Implementando Factory de calculo de juros

--- CalcTest.Domain/Business/Factory/CalculoDeJurosFactory.cs
+++ CalcTest.Domain/Business/Factory/CalculoDeJurosFactory.cs
@@ -1,21 +1,32 @@
-﻿using System;
-using CalcTest.Domain.Business.Factory.Interfaces;
+﻿using CalcTest.Domain.Business.Factory.Interfaces;
 using CalcTest.Domain.Business.Interfaces;
 using CalcTest.Domain.Repository;
 
 namespace CalcTest.Domain.Business.Factory
 {
+    /// <summary>
+    /// Classe responsável por constuir implementações concretas de cálculo de juros
+    /// </summary>
     public class CalculoDeJurosFactory : ICalculoDeJurosFactory
     {
         private readonly ITaxaDeJurosRepository _taxaDeJurosRepository;
 
+        /// <summary>
+        /// Construtor responsável por inicializar as dependências necessárias para criação das classes de cálculo de juros
+        /// </summary>
+        /// <param name="taxaDeJurosRepository">Repoisitorio de taxas de juros usado para selecionar a taxa atualizada</param>
         public CalculoDeJurosFactory(ITaxaDeJurosRepository taxaDeJurosRepository)
         {
             _taxaDeJurosRepository = taxaDeJurosRepository;
         }
+
+        /// <summary>
+        /// Cria instância da classe de cálculo de juros compostos
+        /// </summary>
+        /// <returns>Implementação concreta da classe de cálculo de juros compostos</returns>
         public ICalculoDeJuros CreateCalculoDeJurosCompostos()
         {
-            throw new NotImplementedException();
+            return new CalculoDeJuros(_taxaDeJurosRepository.SelecionarTaxaDeJurosAtualizada());
         }
     }
 }
--- CalcTest.Domain/Business/Factory/Interfaces/ICalculoDeJurosFactory.cs
+++ CalcTest.Domain/Business/Factory/Interfaces/ICalculoDeJurosFactory.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using CalcTest.Domain.Business.Interfaces;
+﻿using CalcTest.Domain.Business.Interfaces;
 
 namespace CalcTest.Domain.Business.Factory.Interfaces
 {
 
+    /// <summary>
+    /// Interface responsável por constuir implementações concretas de cálculo de juros
+    /// </summary>
     public interface ICalculoDeJurosFactory
     {
+        /// <summary>
+        /// Cria instância da classe de cálculo de juros compostos
+        /// </summary>
+        /// <returns>Implementação concreta da classe de cálculo de juros compostos</returns>
         ICalculoDeJuros CreateCalculoDeJurosCompostos();
     }
 }
